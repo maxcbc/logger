b6afd7fbe8086301676f26ff9d2f41fc9f63d21f
Max Bladen-Clark <max@buzzard.dev>
2019-08-09 16:29:47 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+const winston = require('winston');
+
+const {NODE_ENV} = process.env;
+
+const DEFAULT_METADATA = {
+    service: 'NOT_SET',
+    env: NODE_ENV
+};
+let initialised = false;
+
+const logger = module.exports = winston.createLogger({
+    defaultMeta: DEFAULT_METADATA,
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.json()
+        })
+    ]
+});
+
+
+logger.init = (options = {}) => {
+    if (!options.service) throw new Error('\'service\' cannot be undefined');
+    initialised = true;
+    Object.assign(DEFAULT_METADATA, options);
+    return logger;
+};
+
+
