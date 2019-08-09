5785f2113ad7193ba659f4db1a63efb411996710
Max Bladen-Clark <max@buzzard.dev>
2019-08-09 20:26:04 +0100

    Add codeowners and lint

--- index.js
+++ index.js
@@ -4,26 +4,24 @@ const winston = require('winston');
 const {NODE_ENV} = process.env;
 
 const DEFAULT_METADATA = {
-    service: 'NOT_SET',
-    env: NODE_ENV
+	service: 'NOT_SET',
+	env: NODE_ENV
 };
-let initialised = false;
 
 const logger = module.exports = winston.createLogger({
-    defaultMeta: DEFAULT_METADATA,
-    transports: [
-        new winston.transports.Console({
-            format: winston.format.json()
-        })
-    ]
+	defaultMeta: DEFAULT_METADATA,
+	transports: [
+		new winston.transports.Console({
+			format: winston.format.json()
+		})
+	]
 });
 
 
 logger.init = (options = {}) => {
-    if (!options.service) throw new Error('\'service\' cannot be undefined');
-    initialised = true;
-    Object.assign(DEFAULT_METADATA, options);
-    return logger;
+	if (!options.service) throw new Error('\'service\' cannot be undefined');
+	Object.assign(DEFAULT_METADATA, options);
+	return logger;
 };
 
 
